e009b941023b0f07779df9a45a39755d7d909d2e
Abdelrhman Rabei <113059575+abdorabie905tag@users.noreply.github.com>
2022-12-30 11:41:38 +0200

    Add files via upload

---
+++ parser.tab.h
@@ -0,0 +1,15 @@
+#ifndef YYSTYPE
+#define YYSTYPE int
+#endif
+#define	Plus	257
+#define	Minus	258
+#define	Number	259
+#define	Equal	260
+#define	Mult	261
+#define	Div	262
+#define	Power	263
+#define	PR	264
+#define	PL	265
+
+
+extern YYSTYPE yylval;
